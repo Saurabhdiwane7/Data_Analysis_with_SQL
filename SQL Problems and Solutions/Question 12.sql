e53aceeafb6154d3c7f1601ed2f4dbe73c5782df
Saurabh Diwane <119566028+Saurabhdiwane7@users.noreply.github.com>
2023-09-18 00:59:33 +0530

    Add files via upload

---
+++ SQL Problems and Solutions/Question 12.sql
@@ -0,0 +1,11 @@
+#Q = Based on cities where our patient lives in, write a query to display the list of unique city starting with a vowel (a, e, i, o, u). Show the result order in ascending by city.
+
+select 
+distinct city 
+from patients
+where city like "a%" 
+or city like "e%"
+or city like "i%"
+or city like "o%"
+or city like "u%"
+order by city asc

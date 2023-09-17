76527482f1c7cd38629188d5b1ca0c5023ae4217
Saurabh Diwane <119566028+Saurabhdiwane7@users.noreply.github.com>
2023-09-18 00:54:42 +0530

    Add files via upload

---
+++ SQL Medium Questions and Answers/Question 13.sql
@@ -0,0 +1,8 @@
+#Q = Write a query to find list of patients first_name, last_name, and allergies from Hamilton where allergies are not null
+
+SELECT 
+first_name,
+last_name,
+allergies
+FROM patients
+WHERE city = "Hamilton" and  allergies is not null

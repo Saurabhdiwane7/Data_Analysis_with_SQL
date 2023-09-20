e2f7fc9a6e0ff63461d9c6f1aa72280e5a6b0765
Saurabh Diwane <119566028+Saurabhdiwane7@users.noreply.github.com>
2023-09-20 12:24:25 +0530

    Add files via upload

---
+++ SQL Hard Questions and answers/Question 6.sql
@@ -0,0 +1,13 @@
+/* # Q = Show patient_id, first_name, last_name, and attending doctor's specialty.
+Show only the patients who has a diagnosis as 'Epilepsy' and the doctor's first name is 'Lisa'
+Check patients, admissions, and doctors tables for required information. */
+
+select 
+patients.patient_id,
+patients.first_name,
+patients.last_name,
+doctors.specialty
+from patients
+join admissions on patients.patient_id = admissions.patient_id
+join doctors on admissions.attending_doctor_id = doctors.doctor_id
+where admissions.diagnosis= "Epilepsy" and doctors.first_name ="Lisa"
